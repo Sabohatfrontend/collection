8e4a6c7be1cc0933b5b25febbb99e89d74c04d90
Sabohatfrontend <ssobirova333@gmail.com>
2023-11-04 09:06:58 +0500

    feat: connection database (Sat, Nov 04, 2023 09:06:58 AM)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose')
 const collections = require('./routes/collections');
 
 const app = express();
@@ -12,8 +13,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/collections',collections);
+app.use('/api/collections', collections);
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}!!!`)
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`connected to db & listening on port ${process.env.PORT}!!!`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
