0e1b2f6ab04b3e5da2501d0855b21b4223c95b32
Sabohatfrontend <ssobirova333@gmail.com>
2023-11-13 15:07:14 +0500

    feat: add isAdmin middleware

---
+++ server/middleware/admin.js
@@ -0,0 +1,7 @@
+module.exports = function(req,res,next){
+    if(!req.user.isAdmin){
+        return res.status(403).send('You don\'t have permission to access this resource');
+    }
+
+    next();
+}
