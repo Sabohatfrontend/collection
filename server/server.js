09ec9c2c12aa865471b408bb391c119b65475b54
Sabohatfrontend <ssobirova333@gmail.com>
2023-11-23 17:42:31 +0500

    fix: comment connection mongodb to test

--- server/server.js
+++ server/server.js
@@ -40,12 +40,13 @@ if (process.env.NODE_ENV === "production") {
     })
 }
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`connected to db & listening on port ${process.env.PORT}!!!`)
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+app.listen(process.env.PORT, () => {
+    console.log(`connected to db & listening on port ${process.env.PORT}!!!`)
+});
+// mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
